7ff77aef8c2ef126f088ce71f7d0abcf7ba61628
mateussantosrodrigues650-hash <leozinnxd00@gmail.com>
2026-07-15 17:20:34 -0300

    update
    Aprendi a usar o def junto com o lower e import time

---
+++ leonardoSales.py
@@ -0,0 +1,68 @@
+'''
+como fritar batata frita
+
+escrevendo o passo a passo:
+
+1: recorte a casca da batata
+2: lave a batata
+3: corte a batata em pedaços
+4: coloque na air fryer
+5: espere 20 a 25 minutos a 200°C
+
+agora simplifique:
+
+'''
+import time
+import time
+import time
+import time
+import time
+import time
+import time
+import time
+import time
+import time
+import time
+
+# o import time (e a quantidade de quantos tem) deixa em cada print, um time de 5 segundos.
+
+def fritar_batata_frita(porção):
+    print('-' * 35)
+    print('\n🍟 Fritando Batata - Sistema Simples 🍟\n') #teste para programador.
+    time.sleep(2)
+    print("1. Pegue a batata, uma faca e uma airfryer.")
+    time.sleep(5)
+    print("2. Descasque a batata com a faca.")
+    time.sleep(5)
+    print("3. Lave a batata na água.")
+    time.sleep(5)
+    print("4. corte a batata em vários pedaços em formato retângulo.")
+    time.sleep(5)
+    print("5. com os pedaços cortados, abra a airfryer e coloque os pedaços.")
+    time.sleep(5)
+    print("6. jogue um pouco de sal na panela da airfryer com as batatas dentro.")
+    time.sleep(5)
+    print("7. feche a airfryer e coloque a 200°C")
+    time.sleep(5)
+    print("8. espere 20 a 25 minutos.")
+    time.sleep(5)
+    print("9. quando pronto, abra a airfryer e pegue com cuidado a batata frita.")
+    time.sleep(5)
+    print("10. pronto, pode comer :)")
+    time.sleep(5)
+    
+    
+    if porção.lower() == 'uma porção':
+        resultado = 'batata frita com porção pequena!'
+    
+    elif porção.lower() == 'duas porções':
+        resultado = 'batata frita com porção média!.'
+ 
+    else:
+        resultado = 'batata frita com porção grande!' 
+    
+    return resultado
+
+meu_lanche = fritar_batata_frita("duas porções")
+print(f'\nmeu lanche vai ser : {meu_lanche}\n')
+print('-' * 35)
